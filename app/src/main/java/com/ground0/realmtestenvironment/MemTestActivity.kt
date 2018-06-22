52f6657b3b0b02b7a38e24969980966444920766
00-00-00 <nexusa8@gmail.com>
2018-06-22 15:20:28 +0530

    Testing

---
+++ app/src/main/java/com/ground0/realmtestenvironment/MemTestActivity.kt
@@ -0,0 +1,20 @@
+package com.ground0.realmtestenvironment
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlin.math.log
+
+/**
+ * Created by 00-00-00 on 22/01/18.
+ */
+
+class MemTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        var set: Set<String> = setOf()
+
+    }
+}
